364c032e3e7eb549fc76c9ba9ec1fdfeb65d43f3
gayafhannah <gayafhannah@gmail.com>
2022-05-19 16:01:40 +0800

    Added some stuff to Utilities.java

--- src/main/java/mcbot/Main.java
+++ src/main/java/mcbot/Main.java
@@ -1,12 +1,20 @@
 package mcbot;
 
+import mcbot.Utilities;
+import java.io.OutputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class Main {
     //Default Server addr/port to use when no arguments used
     private static String defaultAddress = "nyaaa.daz.cat";
     private static String defaultPort = "25565";
     private static String defaultUsername = "cunt";
 
-    public static void main(String[] args) {
+    private static String defaultProtocol = "753"; // 1.16.3 //Maybe later delete this if possible since hardcoding it sucks
+
+    public static void main(String[] args) throws IOException {
         System.out.println("Minecraft Bot thing lmao!");
         System.out.println("--------");
 
@@ -36,5 +44,11 @@ public class Main {
         System.out.println("Port: " + port);
         System.out.println("Name: " + name);
         System.out.println("--------");
+
+        /* Test Utilities.writeVarLong() because its weird and i had to cast stuff lol
+        OutputStream f = new FileOutputStream("test.bin");
+        ByteArrayOutputStream b = new ByteArrayOutputStream();
+        Utilities.writeVarLong(2147483647,b);
+        b.writeTo(f);*/
     }
 }
---
+++ src/main/java/mcbot/Utilities.java
@@ -0,0 +1,69 @@
+package mcbot;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+
+public class Utilities {
+    private static final int SEGMENT_BITS = 0x7F;
+    private static final int CONTINUE_BIT = 0x80;
+
+    public static int readVarInt(ByteArrayInputStream inputStream) throws IOException {
+        int value = 0;
+        int position = 0;
+        byte currentByte;
+        while (true) {
+            currentByte = (byte)inputStream.read();
+            value |= (currentByte & SEGMENT_BITS) << position;
+            if ((currentByte & CONTINUE_BIT) == 0) break;
+                position += 7;
+            if (position >= 32) throw new RuntimeException("VarInt is too big");
+    }
+    return value;
+    }
+
+    public static long readVarLong(ByteArrayInputStream inputStream) throws IOException {
+        long value = 0;
+        int position = 0;
+        byte currentByte;
+        while (true) {
+            currentByte = (byte)inputStream.read();
+            value |= (currentByte & SEGMENT_BITS) << position;
+            if ((currentByte & CONTINUE_BIT) == 0) break;
+            position += 7;
+            if (position >= 64) throw new RuntimeException("VarLong is too big");
+        }
+        return value;
+    }
+
+    public static void writeVarInt(int value, ByteArrayOutputStream outputStream) throws IOException {
+        while (true) {
+            if ((value & ~SEGMENT_BITS) == 0) {
+                outputStream.write(value);
+                return;
+            }
+            outputStream.write((value & SEGMENT_BITS) | CONTINUE_BIT);
+            // Note: >>> means that the sign bit is shifted with the rest of the number rather than being left alone
+            value >>>= 7;
+        }
+    }
+
+    public static void writeVarLong(long value, ByteArrayOutputStream outputStream) throws IOException {
+        while (true) {
+            if ((value & ~((long) SEGMENT_BITS)) == 0) {
+                outputStream.write((byte)value); //Testing casting to BYTE
+                return;
+            }
+            outputStream.write(((byte)value & SEGMENT_BITS) | CONTINUE_BIT); //Test casting to BYTE
+            // Note: >>> means that the sign bit is shifted with the rest of the number rather than being left alone
+            value >>>= 7;
+        }
+    }
+
+    public static void writeString(String s, ByteArrayOutputStream outputStream) throws IOException {
+        int length = s.length();
+        writeVarInt(length,outputStream);
+        outputStream.write(s.getBytes("UTF-8"));
+    }
+}
